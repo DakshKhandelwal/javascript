56b11bd72a8b3c501248ee5d255b533d9543b6ea
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2025-04-22 23:08:06 +0530

    Day9

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -61,3 +61,111 @@ let myButton3 = document.querySelectorAll(".myClass");  //all matching elements 
 // 3. InnerHTML: Returns the plain text or HTML content in the element 
 // 4. textContent: Returns textual content even for the hidden elements 
 
+// let div = document.querySelector("div");
+// console.dir(div);
+
+// let heading = document.querySelector("h1");
+
+// let h2 = document.querySelector("h2");
+// console.log(h2.innerText);
+
+// h2.innerText = `${h2.innerText} - Daksh`;  //concatenation of string
+
+// let divs = document.querySelectorAll(".box");
+
+// // divs[0].innerHTML = "Random text in div 1";
+// // divs[1].innerHTML = "Random text in div 2";
+// // divs[2].innerHTML = "Random text in div 3";
+
+// let index = 1;
+
+// for(let div of divs) {
+//     // console.log(div.innerText);
+//     div.innerText = `Random text in div ${index}`;
+//     index++; 
+// }
+
+
+// -----------------------------------------------------
+// DOM Manipualtion
+// Attributes
+// 1. getAttributes(attr) // use this to get value of the atribute
+// 2. setAttribute(attr, value) // used to set the attribute with that value
+
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+// let name = div.getAttribute("name");
+// console.log(name);
+
+// let p = document.querySelector("p");
+// console.log(p.getAttribute("class"));
+
+// let para1 = document.querySelector("p");
+// console.log(para1.setAttribute("class", "newClassRandom"));
+
+
+//Style to access all the inline css of pur node
+
+// // node.style 
+// let div = document.querySelector("div");
+
+// div.style.backgroundColor = "red";
+
+// div.style.fontSize = '24px';
+// // div.style.display = "none";
+// div.innerText = "Hello world"
+
+
+// DOM Manipulation
+// Insert Elements : Done in 2 parts
+// 1. Create a new element (document.createElement("div");)
+// 2. Append that element in out DOM tree 
+
+// Methods to insert the element 
+// 1. element.append(el) //add at the end of the node (inside)
+// 2. element.prepend(el) //adds the the start of our node (inside) 
+// 3. element.before(el) // adds before the node (outside)
+// 4. element.after(el) //adds after the node (outside)
+
+// let newButton = document.createElement("button");
+// newButton.innerText = "click me";
+// console.log(newButton);
+
+// let div = document.querySelector("div");
+// div.append(newButton);
+
+// div.remove();
+
+//Methods to delete elements from the DOM
+//element.remove() // removes the node
+
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i> Hi I am a new element </i>";
+
+// document.querySelector("body").prepend(newHeading);
+
+// let para2 = document.querySelector("p");
+// para2.remove();
+
+// newHeading.remove();
+// -----------------------------------------------------------------------
+// HW Find and read about these methods
+// appendChild()
+// removeChild()
+// -----------------------------------------------------------------------------
+
+// Question: Create a new button. Give it a text "click me", backgroundcolor of red and text color as white . 
+// Insert the button as first element inside the body tag.
+
+let myButton5 = document.createElement("button");
+myButton5.innerText = "click me";
+myButton5.style.backgroundColor = "red";
+myButton5.style.color = "white";
+
+console.log(myButton5);
+
+document.querySelector("body").prepend(myButton5);
